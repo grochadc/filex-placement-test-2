8cf7715086031c5e0740bcbfe3ff6e9ae63052c5
Gonzalo Rocha <grochadc@gmail.com>
2020-09-01 11:39:43 -0500

    Delete Flor link. She will be in a meeting

--- src/store.js
+++ src/store.js
@@ -9,7 +9,7 @@ const linksMorning = [
   "https://meet.google.com/kzx-emdw-tac",
   "https://meet.google.com/wqf-dmjq-qza",
   "https://meet.google.com/zym-djkh-kmc",
-  "https://meet.google.com/ucv-dwyu-reh",
+  "https://meet.google.com/huz-dbiq-hry",
   "https://meet.google.com/huz-dbiq-hry"
 ];
 const linksAfternoon = [
