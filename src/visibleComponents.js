110a45cc3c3629635df5984daa30874c1d9ec684
Gonzalo Rocha <grochadc@gmail.com>
2020-09-05 10:24:22 -0500

    Make sure meet links come from the db

--- src/visibleComponents.js
+++ src/visibleComponents.js
@@ -19,8 +19,7 @@ const VisibleResult = connect(mapStateToPropsResult)(Result);
 
 const VisibleRouter = connect(state => {
   let props = {
-    route: state.route,
-    meetLink: state.meetLinks.currentLink
+    route: state.route
   };
   return props;
 })(Router);
