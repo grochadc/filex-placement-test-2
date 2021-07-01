37a20b6ca51721b7cddae136219d9dfbbeaf542f
Gonzalo Rocha <grochadc@gmail.com>
2021-06-30 19:42:27 -0500

    Update tests on Dashboard component

--- src/components/Dashboard.test.tsx
+++ src/components/Dashboard.test.tsx
@@ -36,7 +36,26 @@ const mocks: Mock[] = [
   {
     request: { query: GET_DEFAULT_SETTINGS },
     result: {
-      data: { isClosed: true, meetLinks: ["link1", "link2", "link3"] },
+      data: {
+        isClosed: true,
+        meetLinks: [
+          {
+            teacher: "Gissel",
+            link: "https://meet.google.com/trk-uzmy-efn",
+            active: false,
+          },
+          {
+            teacher: "Sergio",
+            link: "https://meet.google.com/omn-hwhu-mtn",
+            active: false,
+          },
+          {
+            teacher: "Carlos",
+            link: "https://meet.google.com/moh-ngxa-fqh",
+            active: false,
+          },
+        ],
+      },
     },
   },
   {
@@ -47,13 +66,46 @@ const mocks: Mock[] = [
   },
 ];
 
-test("Dashboard", async () => {
-  render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <Dashboard />
-    </MockedProvider>
-  );
+describe("Dashboard", async () => {
+  beforeEach(async () => {
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Dashboard />
+      </MockedProvider>
+    );
+    await act(
+      async () => await new Promise((resolve) => setTimeout(resolve, 0))
+    );
+  });
 
-  await act(async () => await new Promise((resolve) => setTimeout(resolve, 0)));
-  expect(0).toBe(0);
+  it("adds an empty link component", async () => {
+    const addLinkButton = await screen.findByRole("button", {
+      name: /add link/i,
+    });
+    userEvent.click(addLinkButton);
+
+    const emptyInputs = await screen.findAllByDisplayValue("");
+    expect(emptyInputs.length).toBe(2);
+  });
+
+  it("removes the first link", async () => {
+    const teacherInput = await screen.findByDisplayValue("Gissel");
+    const linkInput = await screen.findByDisplayValue(
+      "https://meet.google.com/trk-uzmy-efn"
+    );
+    const removeButtons = await screen.findAllByRole("button", {
+      name: /remove link/i,
+    });
+    expect(teacherInput).toBeInTheDocument();
+    expect(linkInput).toBeInTheDocument();
+
+    userEvent.click(removeButtons[0]);
+
+    const removedTeacher = screen.queryByDisplayValue("Gissel");
+    const removedLink = screen.queryByDisplayValue(
+      "https://meet.google.com/trk-uzmy-efn"
+    );
+    expect(removedTeacher).toBeNull();
+    expect(removedLink).toBeNull();
+  });
 });
